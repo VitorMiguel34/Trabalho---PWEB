8d0e92140323c2317b1b112a8e00d6958014c660
Victor <miguelvictor035@gmail.com>
2025-12-03 18:54:45 -0300

    fix: corrigindo importacao da classe de validacao no script do cadastro

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,5 +1,5 @@
 import Usuario from '../scripts/Sistema.js'
-import Validaçao from '../scripts/Validacao.js'
+import Validacao from '../scripts/Validacao.js'
 
 //Tenta pegar a lista de usuarios na local storage, se nao existir cria uma lista vazia
 let listaDeUsuarios = JSON.parse(localStorage.getItem("listaDeUsuarios")) || {}
