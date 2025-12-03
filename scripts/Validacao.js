8cf4d9374e4206a82f6804f03e7701651c8b2925
Victor <miguelvictor035@gmail.com>
2025-12-03 18:10:17 -0300

    fix: corrigindo nome da variavel na validacoa do nome do usuario

--- scripts/Validacao.js
+++ scripts/Validacao.js
@@ -1,31 +1,28 @@
-
 export default class Validaçao{
 
     static ConfirmacaoDaSenha(senha, confirmacaoDaSenha){
-
+        
         if(senha.trim() === confirmacaoDaSenha.trim()){
             return true
         }
         return false
-        }
+    }
 
     static Matricula(matricula){
-
         if( (matricula.length == 10) && (matricula.slice(0, 3) == '202' )){
             return true 
         } 
         return false
-        }
+    }
 
     static Nome(nome){
 
-        let static  = true
+        let nomeValido = true
         for( let i of nome.spllit(' ') ){
-
             if (!(( i.trim() == i) && ( i[0].toUpperCase() == i[0]))){
-                static  = false
+                nomeValido  = false
             } 
-            return static 
+            return nomeValido
         }
     }
 }
